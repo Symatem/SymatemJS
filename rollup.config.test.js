cee4940524a2c409ee942e4cd7efdcd2f201d4a2
Markus Felten <Markus.Felten@gmx.de>
2017-06-10 18:57:49 +0200

    chore: use kebab names for test files

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  entry: 'tests/**/*_test.js',
+  entry: 'tests/**/*test.js',
   external: ['ava'],
   plugins: [
     babel({
