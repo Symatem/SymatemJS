066961ff37fb26e0a5a4df5662a6359b10f6f150
Alexander Meißner <AlexanderMeissner@gmx.net>
2018-11-15 10:29:01 +0100

    Added addReplaceOperation(), getPreOffset() and getOperations() methods

--- VersionControl.js
+++ VersionControl.js
@@ -60,7 +60,25 @@ export class Differential {
             }
     }
 
-    getIntermediateOffset(symbol, offset) {
+    addReplaceOperation(dstSymbol, dstOffset, srcSymbol, srcOffset, length) {
+        if(length == 0)
+            return;
+        const entrySymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
+        this.versionControl.ontology.setTriple([this.symbol, BasicBackend.symbolByName.Replace, entrySymbol], true);
+        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Destination, dstSymbol], true);
+        const dstOffsetSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
+        this.versionControl.ontology.setData(dstOffsetSymbol, dstOffset);
+        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.DestinationOffset, dstOffsetSymbol], true);
+        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Source, srcSymbol], true);
+        const srcOffsetSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
+        this.versionControl.ontology.setData(srcOffsetSymbol, srcOffset);
+        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.SourceOffset, srcOffsetSymbol], true);
+        const lengthSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
+        this.versionControl.ontology.setData(lengthSymbol, length);
+        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Length, lengthSymbol], true);
+    }
+
+    getIntermediateOffset(symbol, postOffset) {
         const operations = [];
         for(const type of [BasicBackend.symbolByName.DecreaseLength, BasicBackend.symbolByName.IncreaseLength])
             for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, type, BasicBackend.symbolByName.Void])) {
@@ -77,14 +95,31 @@ export class Differential {
                 });
             }
         operations.sort((a, b) => a.dstOffset-b.dstOffset);
+        let intermediateOffset = postOffset;
         for(let i = 0; i < operations.length; ++i) {
             const operation = operations[i];
             if(operation.length < 0)
-                offset -= operation.length;
-            if(offset < operation.dstOffset)
-                return [offset, operations, i];
+                intermediateOffset -= operation.length;
+            if(intermediateOffset < operation.dstOffset)
+                return [intermediateOffset, operations, i];
         }
-        return [offset, operations, operations.length];
+        return [intermediateOffset, operations, operations.length];
+    }
+
+    getPreOffset(symbol, postOffset) {
+        const [intermediateOffset, creaseLengthOperations, operationIndex] = this.getIntermediateOffset(symbol, postOffset);
+        let preOffset = intermediateOffset;
+        if(operationIndex > 0) {
+            const operationAtIntermediateOffset = creaseLengthOperations[operationIndex-1];
+            if(operationAtIntermediateOffset.length > 0)
+                preOffset -= Math.min(operationAtIntermediateOffset.length, intermediateOffset-operationAtIntermediateOffset.dstOffset);
+        }
+        for(let i = 0; i < operationIndex-1; ++i) {
+            const operation = creaseLengthOperations[i];
+            if(operation.length > 0)
+                preOffset -= operation.length;
+        }
+        return preOffset;
     }
 
     creaseLength(dstSymbol, dstOffset, length) {
@@ -106,20 +141,8 @@ export class Differential {
         if(length == 0)
             return;
         const dstIntermediateOffset = this.getIntermediateOffset(dstSymbol, dstOffset)[0],
-              srcIntermediateOffset = this.getIntermediateOffset(srcSymbol, srcOffset)[0];
-        const entrySymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
-        this.versionControl.ontology.setTriple([this.symbol, BasicBackend.symbolByName.Replace, entrySymbol], true);
-        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Destination, dstSymbol], true);
-        const dstOffsetSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
-        this.versionControl.ontology.setData(dstOffsetSymbol, dstIntermediateOffset);
-        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.DestinationOffset, dstOffsetSymbol], true);
-        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Source, srcSymbol], true);
-        const srcOffsetSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
-        this.versionControl.ontology.setData(srcOffsetSymbol, srcIntermediateOffset);
-        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.SourceOffset, srcOffsetSymbol], true);
-        const lengthSymbol = this.versionControl.ontology.createSymbol(this.versionControl.namespaceId);
-        this.versionControl.ontology.setData(lengthSymbol, length);
-        this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Length, lengthSymbol], true);
+              srcPreOffset = this.getPreOffset(srcSymbol, srcOffset);
+        this.addReplaceOperation(dstSymbol, dstIntermediateOffset, srcSymbol, srcPreOffset, length);
     }
 
     writeData(dstSymbol, offset, length, dataBytes) {
@@ -149,89 +172,63 @@ export class Differential {
         this.versionControl.ontology.setTriple([entrySymbol, BasicBackend.symbolByName.Value, triple[2]], true);
     }
 
-    apply(forwardDiff, backwardDiff = null, relocationTable = {}) {
-        forwardDiff = this; // TODO
-        console.log('Differential', forwardDiff.symbol);
-        const setSymbols = (linked) => {
-            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [forwardDiff.symbol, BasicBackend.symbolByName[(linked) ? 'Create' : 'Release'], BasicBackend.symbolByName.Void])) {
-                console.log((linked) ? 'Create' : 'Release', opTriple[2]);
-                // this.versionControl.ontology.createSymbol(opTriple[2]);
-                // this.versionControl.ontology.releaseSymbol(opTriple[2]);
-            }
+    getOperations(relocationTable = {}) {
+        const operations = {
+            'Draft': {},
+            'Create': [],
+            'Release': [],
+            'Rename': {},
+            'CreaseLength': {},
+            'Replace': {},
+            'Link': [],
+            'Unlink': []
         };
-        const setTriples = (linked) => {
-            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [forwardDiff.symbol, BasicBackend.symbolByName[(linked) ? 'Link' : 'Unlink'], BasicBackend.symbolByName.Void])) {
+        for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName.Draft, BasicBackend.symbolByName.Void]))
+            operations.Draft[opTriple[2]] = this.versionControl.ontology.getRawData(opTriple[2]);
+        for(const type of ['Create', 'Release'])
+            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName[type], BasicBackend.symbolByName.Void]))
+                operations[type].push(opTriple[2]);
+        for(const type of ['Link', 'Unlink'])
+            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName[type], BasicBackend.symbolByName.Void])) {
                 const triple = [
                     BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Entity), relocationTable),
                     BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Attribute), relocationTable),
                     BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Value), relocationTable)
                 ];
-                console.log((linked) ? 'Link' : 'Unlink', triple);
-                // this.versionControl.ontology.setTriple(triple, linked);
+                operations[type].push(triple);
             }
-        };
-        const renameSymbols = () => {
-            const renamingTable = {};
-            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [forwardDiff.symbol, BasicBackend.symbolByName.Rename, BasicBackend.symbolByName.Void])) {
-                const dstSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Destination), relocationTable),
-                      srcSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Source), relocationTable);
-                renamingTable[srcSymbol] = dstSymbol;
-                console.log('Rename', srcSymbol, dstSymbol);
-            }
-            // this.versionControl.ontology.renameSymbols(renamingTable);
-        };
-        const creaseLength = (increase) => {
-            const operations = {};
-            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [forwardDiff.symbol, BasicBackend.symbolByName[(increase) ? 'IncreaseLength' : 'DecreaseLength'], BasicBackend.symbolByName.Void])) {
+        for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName.Rename, BasicBackend.symbolByName.Void])) {
+            const dstSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Destination), relocationTable),
+                  srcSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Source), relocationTable);
+            operations.Rename[srcSymbol] = dstSymbol;
+        }
+        for(const increase of [true, false]) {
+            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName[(increase) ? 'IncreaseLength' : 'DecreaseLength'], BasicBackend.symbolByName.Void])) {
                 const dstSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Destination), relocationTable),
                       dstOffsetSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.DestinationOffset),
                       dstOffset = this.versionControl.ontology.getData(dstOffsetSymbol),
                       lengthSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Length),
                       length = this.versionControl.ontology.getData(lengthSymbol);
-                if(backwardDiff && !increase)
-                    backwardDiff.writeData(dstSymbol, dstOffset, length, this.versionControl.ontology.readData(dstSymbol, dstOffset, length));
-                if(!operations[dstSymbol])
-                    operations[dstSymbol] = [];
-                operations[dstSymbol].push({'dstSymbol': dstSymbol, 'dstOffset': dstOffset, 'length': (increase) ? length : -length});
+                if(!operations.CreaseLength[dstSymbol])
+                    operations.CreaseLength[dstSymbol] = [];
+                operations.CreaseLength[dstSymbol].push({'dstSymbol': dstSymbol, 'dstOffset': dstOffset, 'length': (increase) ? length : -length});
             }
-            for(const dstSymbol in operations) {
-                operations[dstSymbol].sort((!increase) ? (a, b) => a.dstOffset-b.dstOffset : (a, b) => b.dstOffset-a.dstOffset);
-                for(const operation of operations[dstSymbol])
-                    console.log('CreaseLength', operation.dstSymbol, operation.dstOffset, operation.length);
-                //     this.versionControl.ontology.creaseLength(operation.dstSymbol, operation.dstOffset, operation.length);
-            }
-        };
-        const replaceData = () => {
-            const operations = {};
-            for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [forwardDiff.symbol, BasicBackend.symbolByName.Replace, BasicBackend.symbolByName.Void])) {
-                const dstSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Destination), relocationTable),
-                      dstOffsetSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.DestinationOffset),
-                      dstOffset = this.versionControl.ontology.getData(dstOffsetSymbol),
-                      srcSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Source), relocationTable),
-                      srcOffsetSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.SourceOffset),
-                      srcOffset = this.versionControl.ontology.getData(srcOffsetSymbol),
-                      lengthSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Length),
-                      length = this.versionControl.ontology.getData(lengthSymbol),
-                      dataBytes = this.versionControl.ontology.readData(srcSymbol, srcOffset, length);
-                if(backwardDiff && !this.versionControl.ontology.getTriple([forwardDiff.symbol, BasicBackend.symbolByName.Draft, srcSymbol]))
-                    backwardDiff.writeData(srcSymbol, srcOffset, length, this.versionControl.ontology.readData(dstSymbol, dstOffset, length));
-                if(!operations[dstSymbol])
-                    operations[dstSymbol] = [];
-                operations[dstSymbol].push({'dstSymbol': dstSymbol, 'dstOffset': dstOffset, 'srcSymbol': srcSymbol, 'srcOffset': srcOffset, 'length': length, 'dataBytes': dataBytes});
-            }
-            for(const dstSymbol in operations)
-                for(const operation of operations[dstSymbol]) {
-                    console.log('Replace', operation.dstSymbol, operation.dstOffset, operation.length, operation.dataBytes);
-                    // this.versionControl.ontology.writeData(operation.dstSymbol, operation.dstOffset, operation.length, operation.dataBytes);
-                }
-        };
-        setSymbols(true);
-        renameSymbols();
-        creaseLength(true);
-        replaceData();
-        setTriples(true);
-        setTriples(false);
-        creaseLength(false);
-        setSymbols(false);
+            for(const dstSymbol in operations.CreaseLength)
+                operations.CreaseLength[dstSymbol].sort((increase) ? (a, b) => b.dstOffset-a.dstOffset : (a, b) => a.dstOffset-b.dstOffset);
+        }
+        for(const opTriple of this.versionControl.ontology.queryTriples(BasicBackend.queryMask.MMV, [this.symbol, BasicBackend.symbolByName.Replace, BasicBackend.symbolByName.Void])) {
+            const dstSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Destination), relocationTable),
+                  dstOffsetSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.DestinationOffset),
+                  dstOffset = this.versionControl.ontology.getData(dstOffsetSymbol),
+                  srcSymbol = BasicBackend.relocateSymbol(this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Source), relocationTable),
+                  srcOffsetSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.SourceOffset),
+                  srcOffset = this.versionControl.ontology.getData(srcOffsetSymbol),
+                  lengthSymbol = this.versionControl.ontology.getSolitary(opTriple[2], BasicBackend.symbolByName.Length),
+                  length = this.versionControl.ontology.getData(lengthSymbol);
+            if(!operations.Replace[dstSymbol])
+                operations.Replace[dstSymbol] = [];
+            operations.Replace[dstSymbol].push({'dstSymbol': dstSymbol, 'dstOffset': dstOffset, 'srcSymbol': srcSymbol, 'srcOffset': srcOffset, 'length': length});
+        }
+        return operations;
     }
 };
